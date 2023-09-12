321175f607548cb1962843d2d9b0e5b17eff09c2
Mani Kumar <mani.kumar@nagarro.com>
2023-09-12 17:09:20 +0530

    React CDN

---
+++ app.js
@@ -0,0 +1,8 @@
+const heading=React.createElement("h1",{
+    id:"heading",
+    xyz:"abc"
+},"Namaste Mani From React Seperately")
+
+const root=ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(heading);
